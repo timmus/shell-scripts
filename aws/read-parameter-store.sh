248631f2375ceeb431a28ee5869b777d77fec6fe
Timmus <tagersea@gmail.com>
2019-10-21 18:30:03 -0700

    use arithmetic evaluation

--- aws/read-parameter-store.sh
+++ aws/read-parameter-store.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 read -r -d '' jq_filter <<-EOT
-	{ NextToken: .NextToken, Parameters: [.Parameters[] | 
+	{ NextToken: .NextToken, Parameters: [.Parameters[] |
 	{ Name: .Name, Value: .Value }] }
 EOT
 
@@ -11,31 +11,31 @@ next_token=null
 token_param=""
 keep_paging=true
 page_count=0
- 
+
 until [ "$keep_paging" == false ] || [ "$page_count" -eq 100 ]; do
   if [ "$next_token" != null ]
-		then
-	    token_param="--starting-token ${next_token}"
-	fi
+    then
+      token_param="--starting-token ${next_token}"
+  fi
 
-	response=$(aws ssm get-parameters-by-path \
-		--path "/" \
-		--recursive \
-		--with-decryption \
-		--output json \
-		--profile ${profile_name} \
-		--region us-west-2 \
-		--max-items 20 \
-		${token_param})
+  response=$(aws ssm get-parameters-by-path \
+    --path "/" \
+    --recursive \
+    --with-decryption \
+    --output json \
+    --profile ${profile_name} \
+    --region us-west-2 \
+    --max-items 20 \
+    ${token_param})
 
-	echo "$response" | jq -r "$jq_filter"
+  echo "$response" | jq -r "$jq_filter"
 
   next_token=$(echo "$response" | jq -r ".NextToken")
 
   if [ "$next_token" == null ]
-	then
-	    keep_paging=false
-	fi
+    then
+      keep_paging=false
+  fi
 
-	let "page_count++"
+  ((page_count++))
 done
